c10dc3e59b819a8b5074685e5a163c62d8af9ac5
paluja <jtp1006@hotmail.com>
2024-03-14 21:18:54 +0100

    para pasar al mac

---
+++ src/parser/expansion/expansion.c
@@ -0,0 +1,8 @@
+//handle ? == status del comando anterior
+// get Key = a lo que hay en despues del dolar, sacamos eso
+//y despues lo comparamos con la key de la lista y si coincide debvolvemos el value
+
+char *envp_key(char *str)
+{
+    
+}
